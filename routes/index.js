28aabe38700e4b8b3e8a1d94bac9ed5ff8afc026
Your Name <you@example.com>
2016-04-28 16:37:41 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: '首頁',
+    who: req.session.who
+  });
+});
+
+router.post('/verify', function(req, res) {
+	if (req.body.username) {
+		req.session.who = req.body.username;
+	}
+	res.render('index', {
+		title : '首頁',
+		who : req.session.who
+	});
+});
+
+module.exports = router;
